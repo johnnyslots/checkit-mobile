634a87583415eba3fc1b1f9bdf05858df4c53aaf
Yoni Slotwinere <yonislots@gmail.com>
2018-04-28 20:18:59 -0400

    preparing for requestRec socket

--- client/components/RequestRec.js
+++ client/components/RequestRec.js
@@ -3,12 +3,68 @@ import { StyleSheet, Text, View, Button, FlatList, TextInput, Picker } from 'rea
 // import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 import { StackNavigator } from 'react-navigation';
 import requestRecStyles from '../styles/requestRec';
-// import axios from 'axios';
+import axios from 'axios';
+import ipAddress from '../utils';
 import socket from '../socket';
 
 export default class RequestRec extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      email: '',
+      category: '',
+      title: '',
+      message: '',
+      incorrectEmail: false
+    }
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const user = this.props.navigation.state.params.user;
+  }
+
+  handleEmailChange(email) {
+    this.setState({email});
+  }
+
+  handleCategoryChange(category) {
+    this.setState({category});
+  }
+
+  handleTitleChange(title) {
+    this.setState({title});
+  }
+
+  handleMessageChange(message) {
+    this.setState({message});
+  }
+
+  handleSubmit() {
+    const userEmail = this.state.email
+    axios.get(`${ipAddress}/api/users/${userEmail}`)
+    .then(res => {
+      if(res.data.email) {
+        socket.emit('requestRec', this.state);
+        this.setState({
+          email: '',
+          category: '',
+          title: '',
+          message: '',
+          incorrectEmail: false
+        })
+      }
+    })
+    .catch(err => {
+      this.setState({incorrectEmail: true})
+      console.log(err)
+    })
   }
 
   render() {
@@ -17,21 +73,31 @@ export default class RequestRec extends Component {
       <View style={requestRecStyles.container}>
         <Text>Request recommendation</Text>
         <TextInput
-          // onChangeText={this.handleCategoryChange}
-          // value={this.state.category}
+          onChangeText={this.handleEmailChange}
+          value={this.state.email}
+          placeholder="Send to (email)"
+        />
+        {
+          this.state.incorrectEmail ?
+          <Text>User doesn't exist</Text>
+          : null
+        }
+        <TextInput
+          onChangeText={this.handleCategoryChange}
+          value={this.state.category}
           placeholder="Category"
         />
         <TextInput
-          // onChangeText={this.handleTitleChange}
-          // value={this.state.title}
+          onChangeText={this.handleTitleChange}
+          value={this.state.title}
           placeholder="Title"
         />
         <TextInput
-          // onChangeText={this.handleNotesChange}
-          // value={this.state.notes}
-          placeholder="Notes"
+          onChangeText={this.handleMessageChange}
+          value={this.state.message}
+          placeholder="Message"
         />
-        <Button onPress={this.handleSubmit} title="Send"/>
+        <Button onPress={this.handleSubmit} title="Send request"/>
       </View>
     )
   }
