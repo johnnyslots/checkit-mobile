7bc48897f99076178a4db0c9ac7722ed14c39d85
Yoni Slotwinere <yonislots@gmail.com>
2018-04-29 16:49:42 -0400

    added google link

--- client/components/bookInfo.js
+++ client/components/bookInfo.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import bookInfoStyles from '../styles/bookInfo';
 import { months } from '../utils';
+// import Hyperlink from 'react-native-hyperlink'
 
 const bookInfo = ({navigation}) => {
   const info = navigation.state.params.bookRec;
   const bookRec = info.bookRec;
   const dateSplit = info.bookRec.createdAt.slice(0, 10).split('-');
   const recommendedAt = `${months[dateSplit[1]]} ${dateSplit[2]}, ${dateSplit[0]}`;
+  const url = bookRec.item.findOnGoogle
 
   return (
   <View style={bookInfoStyles.container}>
@@ -19,6 +21,7 @@ const bookInfo = ({navigation}) => {
       : <Text>Recommended by me on {recommendedAt}</Text>
     }
     <Text>Notes: {bookRec.notes}</Text>
+    <Text style={{color: 'blue'}} onPress={() => Linking.openURL(url)}>Find it on Google</Text>
   </View>
   )
 }
